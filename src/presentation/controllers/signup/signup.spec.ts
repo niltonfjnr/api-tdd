a334bc3dd70ef6fff873190c562cff7d64d3a444
Nilton F J <niltonfjr086@gmail.com>
2020-11-12 17:05:44 -0300

    test: ensure SignUpController returns 200 if valida data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -30,7 +30,7 @@ const makeAddAccount = (): AddAccount => {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
-        email: 'valid_email@mail.com',
+        email: 'valid@mail.com',
         password: 'valid_password'
       }
       return fakeAccount
@@ -211,6 +211,28 @@ describe('SignUp Controller', () => {
       name: 'any_name',
       email: 'any@mail.com',
       password: 'any_password'
+    })
+  })
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid@mail.com',
+      password: 'valid_password'
     })
   })
 })
