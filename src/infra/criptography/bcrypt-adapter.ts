dce9c28460f851cf9b90a0259ad0d876bacca643
Nilton F J <niltonfjr086@gmail.com>
2020-11-17 15:28:24 -0300

    docs: optional try catch to examplify an internal error throw

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -5,7 +5,13 @@ export class BcryptAdapter implements Encrypter {
   constructor (private readonly salt: number) { }
 
   async encrypt (value: string): Promise<string> {
-    const hashedValue = await bcrypt.hash(value, this.salt)
-    return hashedValue
+    /**
+     * This try catch is optional, just simulating cathing an error e throwing it again
+     * in a Promise structure
+     */
+    try {
+      const hashedValue = await bcrypt.hash(value, this.salt)
+      return hashedValue
+    } catch (e) { return await new Promise((resolve, reject) => reject(e)) }
   }
 }
