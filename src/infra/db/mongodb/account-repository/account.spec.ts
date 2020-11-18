83c628013f40744cf79616f76cbee00c7986a65a
Nilton F J <niltonfjr086@gmail.com>
2020-11-18 15:09:32 -0300

    refactor: remove database table record before each test

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -15,6 +15,10 @@ describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
