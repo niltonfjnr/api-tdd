a687ce3c94bede50e630442a20414bee5580bedd
Nilton F J <niltonfjr086@gmail.com>
2020-11-24 15:33:46 -0300

    chore: adjust jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coveragePathIgnorePatterns: ['protocols/*', 'domain/*'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
