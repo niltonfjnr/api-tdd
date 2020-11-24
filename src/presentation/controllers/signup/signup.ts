190b9595b3caea9bd08d64931a0cd810dc6b2b94
Nilton F J <niltonfjr086@gmail.com>
2020-11-24 17:33:06 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -36,7 +36,6 @@ export class SignUpController implements Controller {
       const account = await this.addAccount.add({ name, email, password })
       return ok(account)
     } catch (error) {
-      console.error(error)
       return serverError(new ServerError())
     }
   }
