2447b8ca5e3454f944e3a9ad5ff4b79bcc960f61
Nilton F J <niltonfjr086@gmail.com>
2020-11-27 15:09:47 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,35 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+interface SutTypes {
+  sut: LogMongoRepository
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: new LogMongoRepository()
+  }
+}
+
+describe('', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should create an error log on success', async () => {
+    const { sut } = makeSut()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
