83d798318b4dc2d6f01efefffb032c4a231e5a07
Nilton F J <niltonfjr086@gmail.com>
2020-11-30 14:22:27 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,20 +1,22 @@
-import { HttpRequest, HttpResponse, Controller, EmailValidator } from './signup-protocols'
+import {
+  HttpRequest, HttpResponse, Controller,
+  EmailValidator, AddAccount, Validation
+} from './signup-protocols'
 
 import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
-import { AddAccount } from '../../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
-  private readonly addAccount: AddAccount
-
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
-    this.emailValidator = emailValidator
-    this.addAccount = addAccount
-  }
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
+
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
