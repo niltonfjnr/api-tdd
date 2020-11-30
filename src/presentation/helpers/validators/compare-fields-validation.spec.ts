65e7f79a1b5c6ec5a4c67c5c494bf68e0aea02c8
Nilton F J <niltonfjr086@gmail.com>
2020-11-30 19:13:00 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,34 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+interface SutTypes {
+  sut: CompareFieldsValidation
+}
+const makeSut = (): SutTypes => {
+  const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+
+  return { sut }
+}
+
+describe('CompareFieldsValidation', () => {
+  test('Should return a MissingParamError if Validation fails', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({
+      field: 'any_name',
+      fieldToCompare: 'wrong_name'
+    })
+
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({
+      field: 'any_name',
+      fieldToCompare: 'any_name'
+    })
+    expect(error).toBeFalsy()
+  })
+})
