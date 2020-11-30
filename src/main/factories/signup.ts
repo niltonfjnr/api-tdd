a56a6bcff6a0c5ca4be732002aab616ccee79cf4
Nilton F J <niltonfjr086@gmail.com>
2020-11-30 18:04:02 -0300

    refactor: use SignUpValidation factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,9 +3,9 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -13,10 +13,7 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
 
-  const validations = []
-  const validationComposite = new ValidationComposite(validations)
-
-  const signUpController = new SignUpController(dbAddAccount, validationComposite)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
 
   const logMongoRepository = new LogMongoRepository()
 
