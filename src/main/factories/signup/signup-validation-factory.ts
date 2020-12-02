2b594f60a51fa993868042c1fb9b8e9a3eef0a20
Nilton F J <niltonfjr086@gmail.com>
2020-12-02 17:35:11 -0300

    refactor: move EmailValidatorAdapter to the main layer

--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -3,7 +3,7 @@ import {
   RequiredFieldValidation, CompareFieldsValidation, EmailValidation, ValidationComposite
 } from '../../../presentation/helpers/validators'
 import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
