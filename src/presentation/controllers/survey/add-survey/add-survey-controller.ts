49f00737deffae73ad61dfa865ed67ca663b1913
Nilton F J <niltonfjr086@gmail.com>
2020-12-10 11:03:39 -0300

    feat: ensure AddSurveyController returns 204 on sucess

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,5 +1,5 @@
 import { AddSurvey } from '../../../../domain/usecases/add-survey'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -18,7 +18,7 @@ export class AddSurveyController implements Controller {
       const { question, answers } = httpRequest.body
       await this.addSurvey.add({ question, answers })
 
-      return await new Promise(resolve => resolve(null as unknown as HttpResponse))
+      return noContent()
     } catch (error) {
       return serverError(error)
     }
