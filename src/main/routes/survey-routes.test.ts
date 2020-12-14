46b1d97f0dddc55e98ff089c28c80002b508553f
Nilton F J <niltonfjr086@gmail.com>
2020-12-14 12:16:33 -0300

    test: ensure load surveys routes returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -35,30 +35,38 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
+
+    test('Should return 204 on add survey wiht valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Merovingian',
+        email: 'merovingian@matrix.com',
+        password: '123',
+        role: 'admin'
+      })
+
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+
+      await request(app)
+        .post('/api/surveys').set({ 'x-access-token': accessToken })
+        .send({
+          question: 'Question',
+          answers: [
+            { answer: 'Answer 1', image: 'http://image-name.com' },
+            { answer: 'Answer 2' }
+          ]
+        })
+        .expect(204)
+    })
   })
 
-  test('Should return 204 on add survey wiht valid accessToken', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Merovingian',
-      email: 'merovingian@matrix.com',
-      password: '123',
-      role: 'admin'
+  describe('GET /surveys', () => {
+    test('Should return 403 on load surveys without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
     })
-
-    const id = res.ops[0]._id
-    const accessToken = sign({ id }, env.jwtSecret)
-
-    await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
-
-    await request(app)
-      .post('/api/surveys').set({ 'x-access-token': accessToken })
-      .send({
-        question: 'Question',
-        answers: [
-          { answer: 'Answer 1', image: 'http://image-name.com' },
-          { answer: 'Answer 2' }
-        ]
-      })
-      .expect(204)
   })
 })
